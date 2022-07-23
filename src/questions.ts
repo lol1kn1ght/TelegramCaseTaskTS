521c906cac12349efd580730d9c2131d48adfb31
lol1kn1ght <loliknight@easygaming.su>
2022-07-23 11:01:59 +0300

    исправлена ошибка

--- src/questions.ts
+++ src/questions.ts
@@ -24,7 +24,7 @@ export class questions_manager {
 
     if (question && !replied_message) {
       const question_data = await this.get_data({
-        question: question.split(' '),
+        question: question,
       });
       if (!question_data) {
         this.write_question(message);
